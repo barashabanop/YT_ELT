ee26896b49f1b1ed77c642f8a237c426e2e60755
Bara Shaban <bara.shaban@optimumpartners.com>
2026-02-23 16:59:22 +0300

    added the .env file

---
+++ video_status.py
@@ -0,0 +1,38 @@
+import requests 
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='./.env')
+API_KEY=os.getenv('API_KEY')
+CHANNEL_HANDLE = 'MrBeast'
+
+def get_playlist_id():
+    try:
+        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
+
+
+
+        response = requests.get(url=url)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        channel_items = data["items"][0]
+
+        channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
+
+        return channel_playlistId
+
+
+    except requests.exceptions.RequestException as e:
+        print('here')
+        raise e
+    except Exception as e :
+        pass
+
+
+
+
+if __name__ =='__main__':
+    print(get_playlist_id())
